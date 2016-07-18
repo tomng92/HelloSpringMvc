baa932328c576812b790b865f8fc4ab54a4f87f5
thanh nguyen <tnguyen@sodec.gouv.qc.ca>
2016-07-18 18:52:12 -0400

    initial

---
+++ README.txt
@@ -0,0 +1,13 @@
+
+
+Ce projet vient de http://www.programcreek.com/2014/02/spring-mvc-helloworld-using-maven-in-eclipse/.
+
+Utiliser maven pour avoir un projet Spring MVC minimal rapidement.
+
+(je voulais aussi JPA, mais viendra apres).
+
+Pour creer un project Github rapidement on command line, use this: https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
+
+
+
+Thanh Nguyen 18 Juillet 2016.
---
+++ src/main/java/com/sodec/HelloSpring.java
@@ -0,0 +1,34 @@
+package com.sodec;
+/*
+ * Sodacces, Société de développement des entreprises culturelles (SODEC).
+ *
+ * TODO Incrire ici toutes informations sur les droits d'utilisation et de modification 
+ * des compostantes du système Sodacces.
+ */
+
+/**
+ * @author tnguyen 2016-07-18
+ * @version 1.0
+ *
+ */
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+ 
+@Controller
+public class HelloSpring {
+	String message = "Welcome to Spring MVC!";
+ 
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+ 
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+}
